7bef09d4c0a52943bf11273d7724bcd4d9dcd151
Lab shared laptop <shrevzen@umich.edu>
2017-01-26 20:58:43 -0500

    moveinchworm program now works, can set rates for the sheetplans, however, needs tweaking because program runs much slower than poseable programming. Added slow and sharp left turn .csv files (works much better in posable programming

--- move_inchbot.py
+++ move_inchbot.py
@@ -6,135 +6,68 @@ This is a temporary script file.
 """
 
 from joy import *
-
-# !! This is only used to go Forward and then Right using one key !! #
-class MoveUpRight( Plan ):
-  """
-  ShaveNHaircutPlan shows a simple example of sequential composition:
-  its behavior is to run the shave plan followed by the haircut plan.
-  """
-  def __init__(self,app,up,right,*arg,**kw):
-    Plan.__init__(self,app,*arg,**kw)
-    self.up = up
-    self.right = right
-    
-  def behavior( self ):
-    progress("Both: Move Foward")
-    yield self.up
-    progress("Both: Move Right")
-    yield self.right
-    progress("Both: done")
     
 class InchMoveApp( JoyApp ):
   # Load both patterns from their CSV files
-  UP = loadCSV("move_up.csv")
-  RIGHT = loadCSV("move_right.csv")
-  LEFT = loadCSV("move_left.csv")
-
-  def __init__(self,upSpec,rightSpec,leftSpec,*arg,**kw):
-    JoyApp.__init__(self, *arg,**kw)
-    self.upSpec = upSpec
-    self.rightSpec = rightSpec
-    self.leftSpec = leftSpec
+  FORWARD = loadCSV("movements/forward.csv")
+  SHARP_LEFT = loadCSV("movements/sharp_left.csv")
+  SLOW_LEFT = loadCSV("movements/slow_left.csv")
+  SHARP_RIGHT = loadCSV("movements/sharp_right.csv")
+  SLOW_RIGHT = loadCSV("movements/slow_right.csv")
 
   def onStart(self):
-    #
-    #
-    self.upplan = SheetPlan(self, self.UP, x=self.upSpec ) 
-    # give us start and stop messages; in your own code you can omit these 
-    self.upplan.onStart = lambda : progress("Move Foward: starting") 
-    self.upplan.onStop = lambda : progress("Move Forward: done") 
-    #
-    #
-    self.rightplan = SheetPlan(self, self.RIGHT, x=self.rightSpec )
-    # give us start and stop messages; in your own code you can omit these 
-    self.rightplan.onStart = lambda : progress("Turn Right: starting") 
-    self.rightplan.onStop = lambda : progress("Turn Right: done") 
-    
-    self.leftplan = SheetPlan(self, self.LEFT, x=self.leftSpec ) 
-    # give us start and stop messages; in your own code you can omit these 
-    self.leftplan.onStart = lambda : progress("Turn Left: starting") 
-    self.leftplan.onStop = lambda : progress("Turn Left: done") 
-    #
-    # Set up a ShaveNHaircutPlan using both of the previous plans
-    #
-    self.up_right = ShaveNHaircutPlan(self, self.upplan, self.rightplan)
+    self.slowleftplan = SheetPlan(self, self.SLOW_LEFT)
+    self.slowleftplan.setRate(2) # 2 means twice as fast
+    self.slowleftplan.onStart = lambda : progress("Slow Left: starting") 
+    self.slowleftplan.onStop = lambda : progress("Slow Left: done") 
+
+    self.slowrightplan = SheetPlan(self, self.SLOW_RIGHT)
+    self.slowrightplan.setRate(2)
+    self.slowrightplan.onStart = lambda : progress("Slow Right: starting") 
+    self.slowrightplan.onStop = lambda : progress("Slow Right: done")
+
+    self.sharpleftplan = SheetPlan(self, self.SHARP_LEFT) 
+    self.sharpleftplan.setRate(2)
+    self.sharpleftplan.onStart = lambda : progress("Sharp Left: starting") 
+    self.sharpleftplan.onStop = lambda : progress("Sharp Left: done") 
+
+    self.sharprightplan = SheetPlan(self, self.SHARP_RIGHT) 
+    self.sharprightplan.setRate(2)
+    self.sharprightplan.onStart = lambda : progress("Sharp Right: starting") 
+    self.sharprightplan.onStop = lambda : progress("Sharp Right: done") 
+
+    self.forwardplan = SheetPlan(self, self.FORWARD) 
+    self.forwardplan.setRate(1)
+    self.forwardplan.onStart = lambda : progress("Forward: starting") 
+    self.forwardplan.onStop = lambda : progress("Forward: done")  
 
   def onEvent(self,evt):
     if evt.type != KEYDOWN:
       return
     # assertion: must be a KEYDOWN event 
-    if evt.key == K_u:
-      if ( not self.rightplan.isRunning()
-           and not self.leftplan.isRunning() ):
-        self.upplan.start()
-    elif evt.key == K_r:
-      if ( not self.upplan.isRunning()
-           and not self.leftplan.isRunning() ):
-        self.rightplan.start()
-    elif evt.key == K_l:
-      if ( not self.upplan.isRunning()
-           and not self.rightplan.isRunning() ):
-        self.leftplan.start()
-    elif evt.key == K_ESCAPE:
+    if evt.key == K_q:
+        self.slowleftplan.start()
+    elif evt.key == K_e:
+        self.slowrightplan.start()
+    elif evt.key == K_a:
+        self.sharpleftplan.start()
+    elif evt.key == K_d:
+        self.slowrightplan.start()
+    elif evt.key == K_w:
+        self.forwardplan.start()
+    elif evt.key == K_s:
         self.stop()
 
 if __name__=="__main__":
   robot = None
   scr = None
-  upSpec = "#up "
-  rightSpec = "#right "
-  leftSpec = "#left "
+
   args = list(sys.argv[1:])
   while args:
     arg = args.pop(0)
     if arg=='--mod-count' or arg=='-c':
       N = int(args.pop(0))
       robot = dict(count=N)
-    elif arg=='--up' or arg=='-u':
-      upSpec = args.pop(0)
-      if upSpec[:1]==">": scr = {}
-    elif arg=='--right' or arg=='-r':
-      rightSpec = args.pop(0)
-      if rightSpec[:1]==">": scr = {}
-    elif arg=='--left' or arg=='-l':
-      leftSpec = args.pop(0)
-      if leftSpec[:1]==">": scr = {}
-    elif arg=='--help' or arg=='-h':
-      sys.stdout.write("""
-  Usage: %s [options]
-  
-    'Shave and a Haircut' example of running Plan-s in parallel and in
-    sequence. The example shows that plans can run in parallel, and that
-    Plan behaviors (e.g. the ShaveNHaircutPlan defined here) can use other
-    plans as sub-behaviors, thereby "calling them" sequentially.
-    
-    When running, the demo uses the keyboard. The keys are:
-      's' -- start "Shave"
-      'h' -- start "Haircut"
-      'b' -- start "Both", calling "Shave" and "Haircut" in sequence
-      'escape' -- exit program
-      
-    Options:      
-      --mod-count <number> | -c <number>
-        Search for specified number of modules at startup
-      
-      --shave <spec> | -s <spec>
-      --haircut <spec> | -h <spec>
-        Specify the output setter to use for 'shave' (resp. 'haircut')
-        
-        Typical <spec> values would be:
-         '#shave ' -- to print messages to the terminal with '#shave ' as prefix
-         '>x' -- send to Scratch sensor 'x'
-         'Nx3C/@set_pos' -- send to position of CKBot servo module with ID 0x3C
-        
-        NOTE: to use robot modules you MUST also specify a -c option
 
-    NOTE NOTE: I was too lazy to modify this, use -u, -r, -l for up, right, left commands
-        
-    """ % sys.argv[0])
-      sys.exit(1)
-    # ENDS cmdline parsing loop
-  
-  app = InchMoveApp(upSpec,rightSpec,leftSpec,robot=robot,scr=scr)
+  app = InchMoveApp(robot = robot, scr = scr)
   app.run()
